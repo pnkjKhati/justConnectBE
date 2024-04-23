02c083d53520d5f74bd024567eb2634d5af2e228
Pankaj Khati <pankaj.khati.45@gmail.com>
2024-04-23 13:06:13 +0530

    Added register and login user route

--- src/app.js
+++ src/app.js
@@ -4,17 +4,27 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  }));
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({
+app.use(express.json({ limit: "16kb" }));
+app.use(
+  express.urlencoded({
     extended: true,
-    limit: "16kb"
-}))
-app.use(express.static("public"))
-app.use(cookieParser())
+    limit: "16kb",
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "dootChat";
+export const DB_NAME = "justConnect";
